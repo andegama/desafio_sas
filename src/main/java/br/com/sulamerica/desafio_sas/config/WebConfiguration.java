71a0d409ad27d742d824106a277c81a6799c0c29
andegama <anderson.gama@tty2000.com.br>
2019-11-07 19:01:06 -0300

    Desabilitando temporariamente o Spring Security

--- src/main/java/br/com/sulamerica/desafio_sas/config/WebConfiguration.java
+++ src/main/java/br/com/sulamerica/desafio_sas/config/WebConfiguration.java
@@ -1,19 +1,17 @@
 package br.com.sulamerica.desafio_sas.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebConfiguration extends WebSecurityConfigurerAdapter{
+public class WebConfiguration /*extends WebSecurityConfigurerAdapter*/{
 
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/h2_console/**").permitAll();
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-    }
+//	@Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/h2_console/**").permitAll();
+//
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+//    }
 }
