1ab1f96994898dd693e0bc633445cd4e6ebb273c
andegama <anderson.gama@tty2000.com.br>
2019-11-06 22:42:27 -0300

    Primeiro Commit - Corpo principal do Projeto

---
+++ src/main/java/br/com/sulamerica/desafio_sas/DesafioSasApplication.java
@@ -0,0 +1,12 @@
+package br.com.sulamerica.desafio_sas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSasApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/sulamerica/desafio_sas/DesafioSasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sulamerica.desafio_sas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
